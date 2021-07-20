135c9d94e37fc57a5b55d7ee2d548d4a8d61b802
iwood55 <isaac.ewood@icloud.com>
2021-07-19 21:57:15 -0400

    flex mvp done

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,28 +4,46 @@ import Episode from './../Episode';
 
 const testEpisode = {
     id:1,
-    name: "",
+    name: "Stranger Things",
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "A great show",
     runtime: 1
 }
 
-const testEpisodeWithoutImage = {
+const testEpisodeWithoutImage = { id:1,
+    name: 'Episode 5',
+    img: '',
+    season: 3,
+    number: 2,
+    summary: 'A cool show',
+    runtime: 1
     //Add in approprate test data structure here.
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode = {[]} />)
 });
 
 test("renders the summury test passed as prop", ()=>{
-    
+    render(<Episode episode = {testEpisode}/>)
+
+    let summary = screen.getByText(`${testEpisode.summary}`)
+
+    expect(summary).toBeInTheDocument();
+    expect(summary).toHaveTextContent(testEpisode.summary);
+    expect(summary).toBeTruthy();
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    render(<Episode episode={testEpisodeWithoutImage} />);
+
+    const info = screen.getByAltText('./stranger_things.png');
+
+    expect(info).toBeInTheDocument();
+    expect(info.alt).toEqual('./stranger_things.png');
+    expect(info).toBeTruthy();
 })
 
 //Tasks
